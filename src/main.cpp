d43672b54ff57cb49a95d2aefd4c128511f8399d
chenhu84 <ht.chen@mail.utoronto.ca>
2021-05-13 18:54:32 -0400

    sometimes work

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@
 #include "utils.h"
 #include "config.h"
 #include <fstream>
+#include "ThreadPool.h"
 
 class Socket {
     int fd;
@@ -52,19 +53,22 @@ public:
     }
     void sendFile(std::string filepath, std::string title, int statusCode=200) {
         auto content = read_file_content(filepath);
-        std::cout << "filepath: " << filepath << std::endl;
+        // std::cout << "filepath: " << filepath << std::endl;
         auto splitted = split_str(filepath, ".");
         std::string type = "plain";
         if(splitted.size() > 0) {
             type = splitted.back();
         }
-        std::cout << "type: " << type << std::endl;
+        // std::cout << "type: " << type << std::endl;
         sendResponse(content, title, type);
     }
     ~Socket(){
         if(fd >= 0){
             close(fd);
         }
+    }
+    int getFd()const{
+        return fd;
     }
 };
 
@@ -96,7 +100,7 @@ public:
             perror("accept");
             return std::nullopt;
         }
-        printf("accepted!\n");
+        // printf("accepted!\n");
         return Socket(new_socket);
     }
     static std::optional<TcpListener> bind(int port, int max_pending) {
@@ -140,72 +144,30 @@ public:
 
 int main() {
     char const*hello = "Hello from server";
+    ThreadPool pool(std::thread::hardware_concurrency() * 2);
     if(auto listener = TcpListener::bind(8000, 10)){
         while(true){
             if(auto socket_opt = listener->accept()) {
-                auto& socket = *socket_opt;
-                const size_t READ_SIZE = 100000;
-                char request[READ_SIZE+1];
-                socket.read(request, READ_SIZE);
-                // printf("%s\n", request);
-                auto path = get_path(request);
-                auto title = get_title_from_path(path);
-                std::cout << path << std::endl;
-                auto filepath = static_file_folder + path;
-                auto content = read_file_content(filepath);
-                socket.sendFile(filepath, title);   // 2
-                // socket.sendContent("<p>hi</p>", title);  // 1
+                std::shared_ptr<Socket> p_socket(new Socket(std::move(*socket_opt)));
+                pool.add([=]()mutable {
+                    auto&socket = *p_socket;
+                    // std::cout << "client socket fd: " << socket.getFd() << std::endl;
+                    const size_t READ_SIZE = 100000;
+                    char request[READ_SIZE+1];
+                    socket.read(request, READ_SIZE);
+                    // printf("request: %s\n", request);
+                    auto path = get_path(request);
+                    auto title = get_title_from_path(path);
+                    // std::cout << path << std::endl;
+                    auto filepath = static_file_folder + path;
+                    // fmt::print("filepath: {}\n", filepath);
+                    auto content = read_file_content(filepath);
+                    socket.sendFile(filepath, title);   // 2
+                    // socket.sendContent("<p>hi</p>", title);  // 1
+                });
             }
         }
     }else{
         printf("cannot create listener\n");
     }
-    // int server_fd, new_socket, valread;
-    // struct sockaddr_in address;
-    // int opt = 1;
-    // int addrlen = sizeof(address);
-    // char buffer[1024] = {0};
-    // char *hello = "Hello from server";
-       
-    // // Creating socket file descriptor
-    // if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    // {
-    //     perror("socket failed");
-    //     exit(EXIT_FAILURE);
-    // }
-       
-    // // Forcefully attaching socket to the port 8080
-    // if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-    //                                               &opt, sizeof(opt)))
-    // {
-    //     perror("setsockopt");
-    //     exit(EXIT_FAILURE);
-    // }
-    // address.sin_family = AF_INET;
-    // address.sin_addr.s_addr = INADDR_ANY;
-    // address.sin_port = htons( PORT );
-       
-    // // Forcefully attaching socket to the port 8080
-    // if (bind(server_fd, (struct sockaddr *)&address, 
-    //                              sizeof(address))<0)
-    // {
-    //     perror("bind failed");
-    //     exit(EXIT_FAILURE);
-    // }
-    // if (listen(server_fd, 3) < 0)
-    // {
-    //     perror("listen");
-    //     exit(EXIT_FAILURE);
-    // }
-    // if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-    //                    (socklen_t*)&addrlen))<0)
-    // {
-    //     perror("accept");
-    //     exit(EXIT_FAILURE);
-    // }
-    // valread = read( new_socket , buffer, 1024);
-    // printf("%s\n",buffer );
-    // send(new_socket , hello , strlen(hello) , 0 );
-    // printf("Hello message sent\n");
-    // return 0;
 }
